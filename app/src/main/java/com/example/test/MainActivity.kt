07b57b170f42bcec333685bb693822fb3b6091fe
Vladislav Nesterov <nesterovvlad100@gmail.com>
2024-09-06 19:18:52 +0300

    hj

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,7 +12,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -28,9 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.test.TempConverter.ViewModelForTempConverter
 import com.example.test.ui.theme.TestTheme
@@ -103,11 +96,11 @@ fun MainScreen(isFahrenheit: Boolean, result: String, convertTemp: (String) -> U
                 )
             }
         }
-        Text( "result = $result",
+        Text( if(result == ""){""} else{  "result = $result"},
             Modifier.padding(16.dp),
             style = MaterialTheme.typography.headlineMedium,)
 
-        Button(onClick = {convertTemp(inputTextState.toString())}) {
+        Button(onClick = {convertTemp(inputTextState)}) {
             Text(text =  "Convert to " +  if (isFahrenheit) "Celcius" else "Fahrenheit")
         }
     }
